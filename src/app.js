f90e23c235217bb3537e6208ef070f9b2608af96
Bryan <bryantello2010@hotmail.com>
2021-09-28 13:44:06 -0600

    Added handlebars

--- src/app.js
+++ src/app.js
@@ -1,15 +1,22 @@
 import express from "express";
-
+import exphbs from 'express-handlebars';
+import path from 'path';
 // Initializations
 export const app = express();
 
 
 // Settings
+
 app.set('port', process.env.PORT || 8080);
-
-
+app.set('view', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+    defaultLayout: 'main'
+}));
+app.set('view engine', '.hbs');
 // Middlewares
-
 
 // Routes
 
