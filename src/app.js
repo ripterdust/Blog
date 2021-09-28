4978e0a22565cea13f8db05e2c6474096f17c287
Bryan <bryantello2010@hotmail.com>
2021-09-28 13:30:59 -0600

    Basic server conf

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+// Initializations
+export const app = express();
+
+
+// Settings
+app.set('port', process.env.PORT || 8080);
+
+
+// Middlewares
+
+
+// Routes
+
